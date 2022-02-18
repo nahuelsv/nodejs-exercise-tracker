0337bda7e1fdccfd64725699314494d588cb6715
Nahuel Silvera <nahuelsilvera.cds@gmail.com>
2022-02-18 10:12:27 -0300

    init: first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const errorHandler = require('errorhandler')
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: "false" }))
+
+app.get("/", (req, res, next) => {
+    res.send("Workings")
+});
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }))
+}else if (process.env.NODE_ENV === 'production') {
+    app.use(errorHandler())
+}  
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on ${listener.address().port}`)
+});
